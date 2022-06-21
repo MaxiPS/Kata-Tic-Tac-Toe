9089bc1b9d5de535845f95ac4af37acf9f5ff0f5
MaxiPS <usr2254@salesianos-lacuesta.net>
2022-06-21 13:18:18 +0100

    refactor

--- test/tic-tac-toe.test.ts
+++ test/tic-tac-toe.test.ts
@@ -3,12 +3,12 @@ import { game } from "../src/tic-tac-toe";
 
 describe("tic-tac-toe should", () => {
   test("be the winner the player X", () => {
-    const moves = ["X", "X", "X", "", "O", "O", "", "", ""];
-    expect(game(moves)).toBe("Player X is the winner!");
+    const table = ["X", "X", "X", "", "O", "O", "", "", ""];
+    expect(game(table)).toBe("Player X is the winner!");
   });
 
   test("be the winner the player O", () => {
-    const moves = ["O", "O", "O", "", "X", "X", "", "", ""];
-    expect(game(moves)).toBe("Player O is the winner!");
+    const table = ["O", "O", "O", "", "X", "X", "", "", ""];
+    expect(game(table)).toBe("Player O is the winner!");
   });
 });
